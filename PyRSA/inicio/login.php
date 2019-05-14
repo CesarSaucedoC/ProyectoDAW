70966407f555968345deef1f595157f0c4b307ae
Cesar Saucedo <cesar@Debian.Debian.com>
2019-05-13 23:42:49 -0500

    Carpeta de proyecto
    En esta carpeta se haran todas las modificaciones

---
+++ PyRSA/inicio/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PyRSA Comunicaciones</title>
+	<link src="../bootstrap/bootstrap-3.4.1/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="../js/jquery-3.3.1.js"></script>
+	<script src="../bootstrap/bootstrap-3.4.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="../bootstrap/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../css/login-style.css">
+</head>
+<body>
+	<div class="main">    
+		<div class="container">
+			<center>
+				<div class="middle">
+      				<div id="login">
+						<form action="javascript:void(0);" method="get">
+							<fieldset class="clearfix">
+					            <p >
+					            	<span class="fa fa-user"></span>
+					            	<input type="text"  Placeholder="Username" required>
+					            </p> <!-- JS because of IE support; better: placeholder="Username" -->
+					            <p>
+					            	<span class="fa fa-lock"></span>
+					            	<input type="password"  Placeholder="Password" required>
+					            </p> <!-- JS because of IE support; better: placeholder="Password" -->
+					            <div>
+					            	<!--<span style="width:48%; text-align:left;  display: inline-block;"><a class="small-text" href="#"></a></span>-->
+					                <span style="width:100%; text-align:right;  display: inline-block;"><input type="submit" value="Sign In"></span>
+					            </div>
+          					</fieldset>
+							<div class="clearfix"></div>
+        				</form>
+        				<div class="clearfix"></div>
+      				</div> <!-- end login -->
+      				<div class="logo">
+      					COMUNICACIONES
+        				<div class="clearfix"></div>
+      				</div>
+      			</div>
+			</center>
+    	</div>
+	</div>
+</body>
+</html>
---
+++ PyRSA/inicio/qryAutentica.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+include_once '../bd/conexion.php';
+
+//cuando se presiona el boto enviar valida al usuario
+if(!isset($_POST["btnEnviar"])) {exit;}
+
+$usr = $_POST['txtUsuario'];
+$pwd = md5($_POST['txtPwd']);
+$strQry = "select * from usuario where usr = '".$usr."' and pwd='$pwd'";
+
+
+//ejecutar query
+$result = mysqli_query($link,$strQry);
+
+//evalua resultado del query
+if(mysqli_num_rows($result)>0){
+	//echo '>>'.$usr;
+	//asignar valores del registro inmediato al vector llamado registro
+	$registro = mysqli_fetch_array($result);
+
+	//asigna valores de usuario
+	$_SESSION['usr'] = $usr;
+	$_SESSION['cat'] = $registro['cat'];
+	header("Location: ./inicioMVC.php");
+}
+else{
+	echo "
+	<script>
+		alert('Usuario y/o contraseña incorrectos');
+		window.location.href = './inicioMVC.php';
+	</script>
+	";
+}
+?>
